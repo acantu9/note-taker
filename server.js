02d3e2bb9ea7b0e84563e0cd60392c40b5faf1c6
acantu9 <adrianaacantu@gmail.com>
2023-10-21 13:40:28 -0500

    created app.get for /, /notes, and /api/notes

--- server.js
+++ server.js
@@ -21,30 +21,3 @@ app.get('/api/notes', (req, res) => {
         res.json(JSON.parse(data));
     });
 });
-
-app.post('/api/notes', (req, res) => {
-    const newNote = req.body;
-    fs.readFile(__dirname + '/db/db.json', 'utf8', (err, data) => {
-        if (err) throw err;
-        const notes = JSON.parse(data);
-        newNote.id = notes.length + 1;
-        notes.push(newNote);
-        fs.writeFile(__dirname + '/db/db.json', JSON.stringify(notes), (err) => {
-            if (err) throw err;
-            res.json(newNote);
-        });
-    });
-});
-
-app.delete('/api/notes/:id', (req, res) => {
-    const noteId = req.params.id;
-    fs.readFile(path.join(__dirname, '/db/db.json'), 'utf8', (err, data) => {
-        if (err) throw err;
-        const notes = JSON.parse(data);
-        const updatedNotes = notes.filter(note => note.id !== parseInt(noteId));
-        fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(updatedNotes), (err) => {
-            if (err) throw err;
-            res.json(updatedNotes);
-        });
-    });
-});
