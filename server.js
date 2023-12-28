949e3a67ff57b8a67e7cc9ccce51e31e688d914b
acantu9 <adrianaacantu@gmail.com>
2023-12-27 20:53:00 -0600

    refractored code

--- server.js
+++ server.js
@@ -11,30 +11,56 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+  });
+
 app.get('/notes', (req, res) => {
     res.sendFile(__dirname + '/public/notes.html');
 });
 
+// app.get('/api/notes', (req, res) => {
+//     fs.readFile(__dirname + '/db/db.json', 'utf8', (err, data) => {
+//         if (err) throw err;
+//         res.json(JSON.parse(data));
+//     });
+// });
+
 app.get('/api/notes', (req, res) => {
-    fs.readFile(__dirname + '/db/db.json', 'utf8', (err, data) => {
-        if (err) throw err;
-        res.json(JSON.parse(data));
-    });
-});
+    // Read the notes from the JSON file
+    const notes = JSON.parse(fs.readFileSync('db.json', 'utf8'));
+    res.json(notes);
+  });
+
+// app.post('/api/notes', (req, res) => {
+//     const newNote = req.body;
+//     fs.readFile(__dirname + '/db/db.json', 'utf8', (err, data) => {
+//         if (err) throw err;
+//         const notes = JSON.parse(data);
+//         newNote.id = notes.length + 1;
+//         notes.push(newNote);
+//         fs.writeFile(__dirname + '/db/db.json', JSON.stringify(notes), (err) => {
+//             if (err) throw err;
+//             res.json(newNote);
+//         });
+//     });
+// });
 
 app.post('/api/notes', (req, res) => {
-    const newNote = req.body;
-    fs.readFile(__dirname + '/db/db.json', 'utf8', (err, data) => {
-        if (err) throw err;
-        const notes = JSON.parse(data);
-        newNote.id = notes.length + 1;
-        notes.push(newNote);
-        fs.writeFile(__dirname + '/db/db.json', JSON.stringify(notes), (err) => {
-            if (err) throw err;
-            res.json(newNote);
-        });
-    });
-});
+    // Read the notes from the JSON file
+    const notes = JSON.parse(fs.readFileSync('db.json', 'utf8'));
+    // Generate a unique ID for the new note
+    const newNote = {
+      id: Date.now(),
+      title: req.body.title,
+      text: req.body.text
+    };
+    // Add the new note to the notes array
+    notes.push(newNote);
+    // Write the updated notes array to the JSON file
+    fs.writeFileSync('db.json', JSON.stringify(notes));
+    res.json(newNote);
+  });
 
 app.delete('/api/notes/:id', (req, res) => {
     const noteId = req.params.id;
