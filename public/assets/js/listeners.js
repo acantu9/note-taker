d73980e0256fe2ad3bfd84edf35846a440bf7ad3
acantu <adrianaacantu@gmail.com>
2024-01-13 11:25:12 -0600

    created listeners.js to handle the buttons in notes.html, referenced buttons and set up templates for listener logic

---
+++ public/assets/js/listeners.js
@@ -0,0 +1,19 @@
+// Event listeners to hadle the buttons in notes.html
+
+// Get references to the buttons
+const saveNoteBtn = document.querySelector('save-note');
+const newNoteBtn = document.querySelector('new-note');
+const clearFormBtn = document.querySelector('clear-btn');
+
+// Add event listeners to the buttons
+saveNoteBtn.addEventListener('click', function() {
+  // Logic for saving a note
+});
+
+newNoteBtn.addEventListener('click', function() {
+  // Logic for creating a new note
+});
+
+clearFormBtn.addEventListener('click', function() {
+  // Logic for clearing the form
+});
