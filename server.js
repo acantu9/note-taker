794928639c2a15ad020da4d1f2ad1d808b09d496
acantu9 <adrianaacantu@gmail.com>
2024-01-15 23:01:35 -0600

    refractored to immediated invoke functions

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express');
-const apiRoutes = require('./routes/api-routes');
-const htmlRoutes = require('./routes/html-routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-app.use(apiRoutes);
-app.use(htmlRoutes);
+
+require('./routes/api-routes')(app);
+require('./routes/html-routes')(app);
 
 app.listen(PORT, () => {
   console.log(`App running on http://localhost:${PORT}`);
